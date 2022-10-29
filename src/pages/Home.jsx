e42aff778247a6be6c49ac0b942f815d6af66a98
unknown <viral.bvminfotech@gmail.com>
2022-10-29 17:31:02 +0530

    complete crud

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,22 +41,10 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 
 const useStyles = makeStyles({
   table: {
-    marginTop: 100,
-    minWidth: 900,
   },
 });
 
@@ -83,7 +71,7 @@ const Home = () => {
           Add User
         </Button>
       </div>
-      <TableContainer component={Paper}>
+      <TableContainer style={{marginLeft: '3%', marginRight: '3%',marginTop: '100px',width: '94%'}} component={Paper}>
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
             <TableRow>
